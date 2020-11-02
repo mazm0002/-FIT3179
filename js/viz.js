086d08c067269286beddf65116a8ad78a6c71717
Munir Azme <61130905+mazm0002@users.noreply.github.com>
2020-11-02 20:21:18 +1100

    Update viz.js

--- js/viz.js
+++ js/viz.js
@@ -59,7 +59,7 @@ d3.csv("https://raw.githubusercontent.com/mazm0002/FIT3179/main/data/US%20GDP.cs
      svg.append("text")
     .attr("class", "y label")
     .attr("text-anchor", "end")
-    .attr("y", 6)
+    .attr("y", 3)
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
     .text("Percentage change in GDP");
